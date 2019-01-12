b5578c8c196ea3117290693c66ba41c03a1a785f
Eric Lease Morgan <eric_morgan@infomotions.com>
2019-01-11 23:44:40 -0500

    Update schema.sql

--- etc/schema.sql
+++ etc/schema.sql
@@ -1,4 +1,12 @@
--- api taskforce
+-- library.sql - a simple bibliographic database
+
+-- Eric Lease Morgan (emorgan@nd.edu)
+-- (c) University of Notre Dame; distributed under a GNU Public License
+
+-- January 11, 2019 - first cut
+
+
+-- faculty
 create table faculty (
     id          INTEGER PRIMARY KEY,
     netid       TEXT,
@@ -12,6 +20,7 @@ create table faculty (
     status      TEXT
 );
 
+-- document identifiers
 create table dois (
     netid  TEXT,
     wosid  TEXT,
