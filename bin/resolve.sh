43e80ca2cf30768591753e09d5c92341125cdf53
Eric Lease Morgan <eric_morgan@infomotions.com>
2019-01-13 23:41:34 -0500

    Added resolve.sh

---
+++ bin/resolve.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# resolve.sh - a front-end to doi2bibliographics.sh
+
+# Eric Lease Morgan <emorgan@nd.edu>
+# (c) University of Notre Dame; distributed under a GNU Public License
+
+# January 13, 2019 - first cut
+
+
+# configure
+SQL="SELECT netid, doi FROM bibliographics WHERE doi > '';"
+DB='./etc/library.db'
+DOI2BIBLIOGRAPHICS='./bin/doi2bibliographics.sh'
+
+# do the work and done
+cat $SQL | sqlite3 $DB | parallel $DOI2BIBLIOGRAPHICS {}
+exit
